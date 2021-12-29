8f88f1e8d705a2a7c1dad91fe0e2b625d5391663
Becca Byrne <aggiebec06@gmail.com>
2021-12-28 19:50:21 -0600

    added two routes in category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,16 +1,35 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const { Category, Product, ProductTag } = require('../../models');
 
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+    // be sure to include its associated Products
+    include: [Product],
+  })
+    .then(categories => res.json(categories))
+    .catch(err => { res.status(500).json(err) })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    // be sure to include its associated Products
+    include: [Product],
+  })
+  .then(categories => {
+    if(!categories) {
+      res.status(404).json({ message: 'No category found with this id' });
+      return;
+    }
+      res.json(categories);
+  })
+  .catch(err => { res.status(500).json(err) })
 });
 
 router.post('/', (req, res) => {
