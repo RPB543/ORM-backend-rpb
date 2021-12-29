4978a863f9bfc74644cecf5a5fea8d63891e2435
Becca Byrne <aggiebec06@gmail.com>
2021-12-28 20:42:04 -0600

    added get product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,13 +6,35 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    // be sure to include its associated Category and Tag data
+    include: [Tag, Category]
+  })
+    .then(products => res.json(products))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    // be sure to include its associated Category and Tag data
+    include: [Category, Tag],
+  })
+    .then(products => {
+      if (!products) {
+        res.status(404).json({ message: 'No product found with this id' });
+        return;
+      }
+      res.json(products);
+    })
+    .catch(err => { res.status(500).json(err) })
 });
 
 // create new product
